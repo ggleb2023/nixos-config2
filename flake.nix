9e0087415cf6901aebb8c7781037b785b20b1e60
gleb <you@example.com>
2025-05-06 00:52:36 +0900

    uwu

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{ 
+description = "Your new nix config";
+
+inputs = {
+
+# Nixpkgs
+nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+# Home manager
+	home-manager.url = "github:nix-community/home-manager/release-24.11";
+	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+# Disko
+disko.url = "github:nix-community/disko/latest";
+disko.inputs.nixpkgs.follows = "nixpkgs";
+
+};
+
+outputs = {
+self, nixpkgs, home-manager, disko, ...
+} 
+
+@ inputs: let
+inherit (self) outputs;
+
+in {
+# NixOS configuration entrypoint
+# Available through 'nixos-rebuild --flake .#your-hostname'
+nixosConfigurations = {
+	nixos = nixpkgs.lib.nixosSystem {
+	system = "x86_64-linux";
+	specialArgs = {
+		inherit inputs outputs;
+		inherit disko;
+	};
+	# > Our main nixos configuration file <
+	modules = [
+		./nixos/configuration.nix
+		disko.nixosModules.default
+	];
+	};
+};
+
+# Standalone home-manager configuration entrypoint
+# Available through 'home-manager --flake .#your-username@your-hostname'
+homeConfigurations = {
+	"gleb@nixos" = home-manager.lib.homeManagerConfiguration {
+	pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+	extraSpecialArgs = {inherit inputs outputs;};
+	# > Our main home-manager configuration file <
+	modules = [./home-manager/home.nix];
+	};
+};
+};
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,287 @@
+# for help type nixos-help :3
+
+{inputs, lib, config, pkgs, ... }:
+
+{
+
+imports =
+[ # Include the results of the hardware scan.
+./hardware-configuration.nix
+./disko.nix
+];
+
+nixpkgs = {
+# You can add overlays here
+overlays = [
+# If you want to use overlays exported from other flakes:
+# neovim-nightly-overlay.overlays.default
+
+# Or define it inline, for example:
+# (final: prev: {
+#   hi = final.hello.overrideAttrs (oldAttrs: {
+#     patches = [ ./change-hello-to-hi.patch ];
+#   });
+# })
+];
+# Configure your nixpkgs instance
+config.allowUnfree = true;
+};
+
+nix = let flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+
+in {
+	settings = {
+	# Enable flakes and new 'nix' command
+	experimental-features = "nix-command flakes";
+	# Opinionated: disable global registry
+	flake-registry = "";
+	# Workaround for https://github.com/NixOS/nix/issues/9574
+	nix-path = config.nix.nixPath;
+	};
+	# Opinionated: disable channels
+	channel.enable = false;
+
+	# Opinionated: make flake registry and nix path match flake inputs
+	registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+	nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+};
+
+#Permit EOLed electron package :( 
+nixpkgs.config.permittedInsecurePackages = [
+"electron-27.3.11"
+];
+
+#NVIDIA 
+hardware.nvidia.prime = {
+# Make sure to use the correct Bus ID values for your system!
+intelBusId = "PCI:0:2:0";
+nvidiaBusId = "PCI:1:0:0";
+sync.enable = true;
+};
+
+# Load nvidia driver for Xorg and Wayland
+services.xserver.videoDrivers = ["nvidia"];
+
+# Enable OpenGL
+hardware.graphics = {
+enable = true;
+};
+
+hardware.nvidia = {
+
+	# Modesetting is required.
+	modesetting.enable = true;
+
+	# Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+	# Enable this if you have graphical corruption issues or application crashes after waking
+	# up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+	# of just the bare essentials.
+	powerManagement.enable = false;
+
+	# Fine-grained power management. Turns off GPU when not in use.
+	# Experimental and only works on modern Nvidia GPUs (Turing or newer).
+	powerManagement.finegrained = false;
+
+	# Use the NVidia open source kernel module (not to be confused with the
+	# independent third-party "nouveau" open source driver).
+	# Support is limited to the Turing and later architectures. Full list of 
+	# supported GPUs is at: 
+	# https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+	# Only available from driver 515.43.04+
+	open = true;
+
+	# Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+	nvidiaSettings = true;
+
+	# Optionally, you may need to select the appropriate driver version for your specific GPU.
+	package = config.boot.kernelPackages.nvidiaPackages.stable;
+};
+
+
+
+
+services= {
+
+xserver = {
+	enable = true;
+	libinput.enable = true;
+	xkb = {
+		layout = "us, ru";
+		variant = "";
+		options = "grp:alt_shift_toggle";
+	};
+};
+
+displayManager.sddm.enable = true;
+desktopManager.plasma6.enable = true;
+
+tailscale = {
+	enable = true;
+
+};
+
+# openssh.enable = true;
+
+i2pd = {
+	enable = true;
+	websocket.enable = true;
+};
+
+sunshine = {
+	enable = true;
+	autoStart = true;
+	capSysAdmin = true;
+	openFirewall = true;
+};
+
+gvfs.enable = true;
+printing.enable = true;
+
+};
+
+programs = {
+
+neovim = {
+	enable = true;
+	defaultEditor = true;
+};
+
+steam = {
+	enable = true;
+	remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+	dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+}; 
+
+# mtr.enable = true;
+
+# gnupg.agent = {
+# enable = true;
+# enableSSHSupport = true;
+# };
+
+kdeconnect.enable = true;
+
+firefox.enable = false;
+
+git.enable = true;
+
+};
+
+environment.systemPackages = with pkgs; [
+	telegram-desktop
+	fastfetch
+	btop
+	nekoray
+	usbutils
+	legcord
+	hyfetch
+	pciutils
+	unrar
+	p7zip
+	unp
+	wineWowPackages.waylandFull
+	qbittorrent
+	qdirstat
+	gimp
+	krita 
+	floorp
+	gcc
+	texlive.combined.scheme-full
+	obs-studio
+	ffmpeg
+	onlyoffice-desktopeditors
+	obsidian
+	mars-mips
+	grim
+	slurp
+	swappy
+	wl-clipboard
+]; 
+
+hardware = {
+
+bluetooth = {
+
+enable = true;
+powerOnBoot = true;
+
+};
+
+opentabletdriver.enable = true;
+
+};
+
+# Bootloader.
+boot.loader.systemd-boot.enable = true;
+boot.loader.efi.canTouchEfiVariables = true;
+
+networking.hostName = "nixos"; # Define your hostname.
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+# Configure network proxy if necessary
+# networking.proxy.default = "http://user:password@proxy:port/";
+# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+# Enable networking
+networking.networkmanager.enable = true;
+
+# Set your time zone.
+time.timeZone = "Asia/Yakutsk";
+
+# Select internationalisation properties.
+i18n.defaultLocale = "en_US.UTF-8";
+
+i18n.extraLocaleSettings = {
+LC_ADDRESS = "en_US.UTF-8";
+LC_IDENTIFICATION = "en_US.UTF-8";
+LC_MEASUREMENT = "en_US.UTF-8";
+LC_MONETARY = "en_US.UTF-8";
+LC_NAME = "en_US.UTF-8";
+LC_NUMERIC = "en_US.UTF-8";
+LC_PAPER = "en_US.UTF-8";
+LC_TELEPHONE = "en_US.UTF-8";
+LC_TIME = "en_US.UTF-8";
+};
+
+# SOUND
+hardware.pulseaudio.enable = false;
+security.rtkit.enable = true;
+services.pipewire = {
+enable = true;
+alsa.enable = true;
+alsa.support32Bit = true;
+pulse.enable = true;
+# If you want to use JACK applications, uncomment this
+#jack.enable = true;
+
+# use the example session manager (no others are packaged yet so this is enabled by default,
+# no need to redefine it in your config for now)
+#media-session.enable = true;
+};
+
+# Define a user account. Don't forget to set a password with ‘passwd’.
+users.users.gleb = {
+isNormalUser = true;
+description = "gleb";
+extraGroups = [ "networkmanager" "wheel"];
+};
+
+virtualisation.virtualbox.host.enable = true;
+virtualisation.virtualbox.host.enableExtensionPack = true;
+
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+# networking.firewall.enable = false;
+
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+system.stateVersion = "24.11"; # Did you read the comment?
+}
+
