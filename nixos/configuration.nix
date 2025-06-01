57a6cfe2d96f67e7b1557c27d7e96bc937b285e3
ggleb2023 <you@example.com>
2025-06-01 20:15:37 +0900

    now using pkgs.adwaita-icon-theme directly

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -60,7 +60,7 @@ services.xserver.enable = true;
 services.xserver.displayManager.gdm.enable = true;
 services.xserver.desktopManager.gnome.enable = true;
 
-  environment.gnome.excludePackages = with pkgs; [
+environment.gnome.excludePackages = with pkgs; [
 gnome-console
   ];
 
