f05360109ad64d6a688402c21b53f997de6a3571
ggleb2023 <you@example.com>
2025-06-03 00:54:39 +0900

    qemu specified package and more . . .

--- nixos/modules/vm.nix
+++ nixos/modules/vm.nix
@@ -24,6 +24,8 @@
     libvirtd = {
       enable = true;
       qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
         swtpm.enable = true;
         ovmf.enable = true;
         ovmf.packages = [ pkgs.OVMFFull.fd ];
