a525024b2b2422850fdf79a58ec6e0ab33350e81
ggleb2023 <you@example.com>
2025-06-07 21:10:12 +0900

    garnix(idk what im doing)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -56,6 +56,9 @@ settings = {
 	
 };
 
+extra-substituters = "https://cache.garnix.io";
+extra-trusted-public-keys = "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
+
 services.xserver.displayManager.gdm.enable = true;
 services.xserver.desktopManager.gnome.enable = true;
 
