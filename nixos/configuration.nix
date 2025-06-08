fef44712bdd917b0f67446edc1531f06239a0443
ggleb2023 <you@example.com>
2025-06-08 23:16:37 +0900

    gpu now using offload mode

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -39,6 +39,9 @@ in {
 
 
 settings = {
+        
+        extra-substituters = ["https://cache.garnix.io"];
+        extra-trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
 
 	# Enable flakes and new 'nix' command
 	experimental-features = "nix-command flakes";
@@ -56,8 +59,6 @@ settings = {
 	
 };
 
-extra-substituters = "https://cache.garnix.io";
-extra-trusted-public-keys = "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
 
 services.xserver.displayManager.gdm.enable = true;
 services.xserver.desktopManager.gnome.enable = true;
@@ -75,50 +76,17 @@ security.polkit.enable = true;
 
 #NVIDIA 
 
-# Enable OpenGL
-hardware = {
-	graphics.enable = true;
-	nvidia.modesetting.enable = true;
-};
+hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 
-services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = ["modesetting" "nvidia" ];
+  hardware.nvidia.open = false;
+  hardware.nvidia.prime = {
+        offload.enable = true;
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
 
-hardware.nvidia.prime = {
-# Make sure to use the correct Bus ID values for your system!
-intelBusId = "PCI:0:2:0";
-nvidiaBusId = "PCI:1:0:0";
-sync.enable = true;
-};
-
-# Load nvidia driver for Xorg and Wayland
-
-hardware.nvidia = {
-
-	# Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-	# Enable this if you have graphical corruption issues or application crashes after waking
-	# up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-	# of just the bare essentials.
-	powerManagement.enable = true;
-
-	# Fine-grained power management. Turns off GPU when not in use.
-	# Experimental and only works on modern Nvidia GPUs (Turing or newer).
-	powerManagement.finegrained = false;
-
-	# Use the NVidia open source kernel module (not to be confused with the
-	# independent third-party "nouveau" open source driver).
-	# Support is limited to the Turing and later architectures. Full list of 
-	# supported GPUs is at: 
-	# https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-	# Only available from driver 515.43.04+
-	open = false;
-
-	# Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-	nvidiaSettings = true;
-
-	# Optionally, you may need to select the appropriate driver version for your specific GPU.
-	package = config.boot.kernelPackages.nvidiaPackages.stable;
-};
 #END OF NVIDIA
 
 services= {
@@ -219,6 +187,7 @@ firefox.enable = false;
 };
 
 environment.systemPackages = with pkgs; [
+        fontforge-gtk
         v2rayn
         zettlr
         pureref
