b62d46a68303f5d1c9b068ba65fbc4c3fe52cf10
ggleb2023 <you@example.com>
2025-06-21 23:32:09 +0900

    new lock file and disabling wpa supplicant

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -264,14 +264,14 @@ opentabletdriver.enable = true;
 };
 
 networking.hostName = "nixos"; # Define your hostname.
-networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+#networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
 # Configure network proxy if necessary
 # networking.proxy.default = "http://user:password@proxy:port/";
 # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
 # Enable networking
-networking.networkmanager.enable = false;
+networking.networkmanager.enable = true;
 
 # Set your time zone.
 time.timeZone = "Asia/Yakutsk";
