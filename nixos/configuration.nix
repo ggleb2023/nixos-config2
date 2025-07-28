1d4bb2559089bfa9a958618c40bf452ca5b884fc
ggleb2023 <you@example.com>
2025-07-28 16:13:07 +0900

    pipewire thingies

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -312,7 +312,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -328,6 +328,8 @@
       "wheel"
     ];
   };
+
+  users.extraUsers.gleb.extraGroups = [ "audio" ];
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
