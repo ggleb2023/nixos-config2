6f09cbac52002280c183322bb3436f377398696d
ggleb2023 <you@example.com>
2025-08-02 14:51:10 +0300

    obs studio not in systemPackages

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -186,6 +186,25 @@
 
   programs = {
 
+    obs-studio = {
+      enable = true;
+
+      # optional Nvidia hardware acceleration
+      package = (
+        pkgs.obs-studio.override {
+          cudaSupport = true;
+        }
+      );
+
+      plugins = with pkgs.obs-studio-plugins; [
+        wlrobs
+        obs-backgroundremoval
+        obs-pipewire-audio-capture
+        obs-gstreamer
+        obs-vkcapture
+      ];
+    };
+
     clash-verge = {
 
       enable = true;
@@ -259,14 +278,6 @@
         ppsspp
       ]
     ))
-    wrapOBS
-    {
-      plugins = with pkgs.obs-studio-plugins; [
-        wlrobs
-        obs-backgroundremoval
-        obs-pipewire-audio-capture
-      ];
-    }
   ];
 
   hardware = {
